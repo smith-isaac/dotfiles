40c626d5d9a8d5d0cc164d43f6c55e2f7c25e6bb
Isaac <smith.isaac.r@gmail.com>
2022-01-11 16:52:56 -0700

    Adding julia LSP

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,7 +1,7 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
 
-local servers = { 'solargraph', 'clangd', 'texlab'}
+local servers = { 'solargraph', 'clangd', 'texlab', 'julials'}
 
 for _, lsp in ipairs(servers) do
     if lsp == 'arduino_language_server' then
