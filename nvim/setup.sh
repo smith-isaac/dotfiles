8bdad5f7786b276abd90e9dd2143274d2bbd0087
Isaac Smith <smithisaacr@gmail.com>
2022-02-01 07:57:11 -0700

    Adding Julia_LS setup to setup.sh

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -15,3 +15,6 @@ for f in vim-latex/*; do
 	ln -s $PWD/$f $HOME/.config/nvim/
     fi
 done
+
+# Setting up Julia Language Server
+julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer")'
