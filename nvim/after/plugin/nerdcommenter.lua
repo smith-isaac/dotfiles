12d637c5813e3e89b7fe2bbb4362936d98323ecd
Isaac Smith <smithisaacr@gmail.com>
2022-11-02 16:27:00 -0600

    Lua-ifying config

--- nvim/after/plugin/nerdcommenter.lua
+++ nvim/after/plugin/nerdcommenter.lua
@@ -3,7 +3,7 @@ vim.g.NERDSpaceDelims = 1
 vim.g.NERDCompactSexyComs = 1
 vim.g.NERDDefaultAlign = 'left'
 vim.g.NERDAltDelims_java = 1
-vim.api.nvim_command [[let g:NERDCustomDelimiters = {'c': {'left': '//'}}]]
+vim.g.NERDCustomDelimiters = {c = {left = '//'}}
 vim.g.NERDCommentEmptyLines = 1
 vim.g.NERDTrimTrailingWhitespace = 1
 vim.g.NERDToggleCheckAllLines = 1
