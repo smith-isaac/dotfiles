db588f26837ca972ac0d7a74d60c33cbf76dc56a
Isaac Smith <smithisaacr@gmail.com>
2022-11-02 16:37:50 -0600

    Caching changes

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -3,7 +3,7 @@ require("handlers").setup()
 -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lspconfig = require('lspconfig')
 
-local servers = { 'solargraph', 'clangd', 'texlab', 'julials', 'vimls', 'bashls', 'sumneko_lua', 'pyright', 'csharp_ls'}
+local servers = { 'solargraph', 'clangd', 'texlab', 'julials', 'vimls', 'bashls', 'sumneko_lua', 'pyright', 'csharp_ls', 'arduino_language_server'}
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
@@ -13,30 +13,30 @@ for _, lsp in ipairs(servers) do
     if lsp == 'texlab' then
         lspconfig[lsp].setup {
             settings = {
-              texlab = {
-                auxDirectory = ".",
-                bibtexFormatter = "texlab",
-                build = {
-                  args = { "-interaction=nonstopmode", "-synctex=1", "%f" },
-                  executable = "pdflatex",
-                  -- executable = "latexmk",
-                  forwardSearchAfter = false,
-                  onSave = false
-                },
-                chktex = {
-                  onEdit = false,
-                  onOpenAndSave = false
-                },
-                diagnosticsDelay = 300,
-                formatterLineLength = 80,
-                forwardSearch = {
-                  args = {}
-                },
-                latexFormatter = "latexindent",
-                latexindent = {
-                  modifyLineBreaks = false
+                texlab = {
+                    auxDirectory = ".",
+                    bibtexFormatter = "texlab",
+                    build = {
+                        args = { "-interaction=nonstopmode", "-synctex=1", "%f" },
+                        executable = "pdflatex",
+                        -- executable = "latexmk",
+                        forwardSearchAfter = false,
+                        onSave = false
+                    },
+                    chktex = {
+                        onEdit = false,
+                        onOpenAndSave = false
+                    },
+                    diagnosticsDelay = 300,
+                    formatterLineLength = 80,
+                    forwardSearch = {
+                        args = {}
+                    },
+                    latexFormatter = "latexindent",
+                    latexindent = {
+                        modifyLineBreaks = false
+                    }
                 }
-              }
             },
             capabilities = require("handlers").capabilities,
             on_attach = require("handlers").on_attach
@@ -44,34 +44,45 @@ for _, lsp in ipairs(servers) do
     elseif lsp == 'sumneko_lua' then
         lspconfig[lsp].setup {
             settings = {
-            Lua = {
-              runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path,
-              },
-              diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'},
-              },
-              workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-              },
-              -- Do not send telemetry data containing a randomized but unique identifier
-              telemetry = {
-                enable = false,
-              },
+                Lua = {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT',
+                        -- Setup your lua path
+                        path = runtime_path,
+                    },
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = {'vim'},
+                    },
+                    workspace = {
+                        -- Make the server aware of Neovim runtime files
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    -- Do not send telemetry data containing a randomized but unique identifier
+                    telemetry = {
+                        enable = false,
+                    },
+                },
             },
-          },
-        capabilities = require("handlers").capabilities,
-        on_attach = require("handlers").on_attach
-      }
+            capabilities = require("handlers").capabilities,
+            on_attach = require("handlers").on_attach
+        }
+    elseif lsp == 'arduino_language_server' then
+        lspconfig[lsp].setup {
+            cmd = {
+                "arduino-language-server",
+                "-cli-config", "$HOME/.arduino15/arduino-cli.yaml",
+                "-cli", "arduino-cli",
+                "-clangd", "clangd"
+            },
+            capabilities = require("handlers").capabilities,
+            on_attach = require("handlers").on_attach
+        }
     else
-    lspconfig[lsp].setup {
-        capabilities = require("handlers").capabilities,
-        on_attach = require("handlers").on_attach
-    }
+        lspconfig[lsp].setup {
+            capabilities = require("handlers").capabilities,
+            on_attach = require("handlers").on_attach
+        }
     end
 end
