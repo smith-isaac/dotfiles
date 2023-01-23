683e98a4373110cabb1110cc65550f5571e5c26e
Isaac Smith <smithisaacr@gmail.com>
2023-01-23 09:08:21 -0700

    LaTeX autocompile

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -1,1 +1,13 @@
 require("isaac.languages").tex()
+
+vim.api.nvim_create_user_command("LaTeXAutoCompile", function()
+
+    vim.api.nvim_create_autocmd("BufWritePost", {
+        group = vim.api.nvim_create_augroup("LaTeX Auto Compile", {clear = true}),
+        pattern = vim.fn.expand('%:t'),
+        callback = function()
+            vim.cmd 'TexlabBuild'
+        end
+    })
+
+end, {})
